d28e675b509cdac0c82e285b88948d59200c13cc
Samuel Nussbaumer <samuel@vouliebear.net>
2019-06-11 10:51:15 +0200

    Extracting css to external file

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,39 +3,21 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import '../index.css'
 
 class Message extends Component {
-  classes = {
-    card: {
-      minWidth: 275
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)'
-    },
-    title: {
-      fontSize: 14
-    },
-    time: {
-      marginBottom: 12,
-      paddingRight: 10,
-      pattingBottom: 10,
-      
-    }
-  };
 
   render() {
     return (
-      <Card className={this.classes.card}>
+      <Card className="card">
         <CardContent>
-          <Typography className={this.classes.title} color="textSecondary" gutterBottom>
+          <Typography className="typo" color="textSecondary" gutterBottom>
             {this.props.sender}
           </Typography>
           <Typography variant="body2" component="p">
             {this.props.body}
           </Typography>
-          <Typography className={this.classes.time} color="textSecondary">
+          <Typography className="time" color="textSecondary">
             {new Date().toLocaleTimeString()}
           </Typography>
         </CardContent>
